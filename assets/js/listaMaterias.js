5e8d85b861af21e1979be9a645daa3f36b4881a3
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2023-01-12 20:10:07 -0300

    Update listaMaterias.js

--- assets/js/listaMaterias.js
+++ assets/js/listaMaterias.js
@@ -19,53 +19,41 @@ function anonima1() {
       'Analise e Proj de Sist', 'Ciência da Comp', 'Banco de Dados', 'Prog. Front-end', 'Prog. Back-end', 'Proj de Vida', 'Educ Financeira',
       'Materia indisponivel'];
 
-    if (diaSemana === 0) {
+    if (diaSemana === 1) {
       return materiasDoDia.innerHTML = 'Não há materias para hoje!';
     } else if (diaSemana === 1) {
       return materiasDoDia.innerHTML +=
-        `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
-      ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
+        `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
+      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
     } else if (diaSemana === 2) {
       return materiasDoDia.innerHTML +=
-        `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
-      ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
-    }
-    else if (diaSemana === 3) {
-      return materiasDoDia.innerHTML +=
-        `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
-      ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
+      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
+      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
     }
     else if (diaSemana === 4) {
       return materiasDoDia.innerHTML +=
-        `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
-      ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
+      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
+      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+    }
+    else if (diaSemana === 4) {
+      return materiasDoDia.innerHTML +=
+      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
+      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
     }
     else if (diaSemana === 5) {
       return materiasDoDia.innerHTML +=
-        `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
-      ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
+      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
+      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
     }
     else if (diaSemana === 6){
       return materiasDoDia.innerHTML +=
-        `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
-      ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
+      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
+      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
     }else{
       return materiasDoDia.innerHTML +=
       `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
     ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
     }
-
   }
-
 }
 anonima1();
-
-/*
-domingo 0
-segunda 1
-terça   2
-quarta  3
-quinta  4
-sexta   5
-sabado  6
-*/
