04cc59d4abd4e5a0ec2f6197c32dd30a19b40297
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2023-01-13 11:55:40 -0300

    Update listaMaterias.js

--- assets/js/listaMaterias.js
+++ assets/js/listaMaterias.js
@@ -1,7 +1,6 @@
 const diaSemana = document.querySelector('.diaSemana');
 const materiasDoDia = document.querySelector('.materiasDoDia')
 
-
 function anonima1() {
 
   const diasSemana = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"];
@@ -19,41 +18,42 @@ function anonima1() {
       'Analise e Proj de Sist', 'Ciência da Comp', 'Banco de Dados', 'Prog. Front-end', 'Prog. Back-end', 'Proj de Vida', 'Educ Financeira',
       'Materia indisponivel'];
 
-    if (diaSemana === 1) {
-      return materiasDoDia.innerHTML = 'Não há materias para hoje!';
-    } else if (diaSemana === 1) {
-      return materiasDoDia.innerHTML +=
-        `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
-      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
-    } else if (diaSemana === 2) {
-      return materiasDoDia.innerHTML +=
-      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
-      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+    const materiasSegunda = `${materias[1]}<br>${materias[2]}<br>
+    ${materias[3]}<br>${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+
+    const materiasTerca = `${materias[1]}<br>${materias[2]}<br>
+    ${materias[3]}<br>${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+
+    const materiasQuarta = `${materias[1]}<br>${materias[2]}<br>
+    ${materias[3]}<br>${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+
+    const materiasQuinta = `${materias[1]}<br>${materias[2]}<br>
+    ${materias[3]}<br>${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+
+    const materiasSexta = `${materias[1]}<br>${materias[2]}<br>
+    ${materias[3]}<br>${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+
+    const materiasIndisponiveis = materias[15]
+
+    if (diaSemana === 0 || diaDaSemana === 6) {
+      return materiasDoDia.innerHTML = 'Não há materias para hoje! Final de semana!';
+    }
+    else if (diaSemana === 1) {
+      return materiasDoDia.innerHTML += materiasSegunda;
+    }
+    else if (diaSemana === 2) {
+      return materiasDoDia.innerHTML += materiasTerca;
+    }
+    else if (diaSemana === 3) {
+      return materiasDoDia.innerHTML += materiasQuarta;
     }
     else if (diaSemana === 4) {
-      return materiasDoDia.innerHTML +=
-      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
-      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
-    }
-    else if (diaSemana === 4) {
-      return materiasDoDia.innerHTML +=
-      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
-      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
+      return materiasDoDia.innerHTML += materiasQuinta;
     }
     else if (diaSemana === 5) {
-      return materiasDoDia.innerHTML +=
-      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
-      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
-    }
-    else if (diaSemana === 6){
-      return materiasDoDia.innerHTML +=
-      `${materias[1]}<br>${materias[2]}<br>${materias[3]}<br>
-      ${materias[4]}<br>${materias[5]}<br>${materias[6]}<br>`;
-    }else{
-      return materiasDoDia.innerHTML +=
-      `${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>
-    ${materias[15]}<br>${materias[15]}<br>${materias[15]}<br>`;
+      return materiasDoDia.innerHTML += materiasSexta;
+    } else {
+      return materiasDoDia.innerHTML += materiasIndisponiveis;
     }
   }
-}
-anonima1();
+} anonima1();
