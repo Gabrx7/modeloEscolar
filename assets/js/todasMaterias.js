00978aa4c24214930d1dae4ca74c2925d14c2a12
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2023-01-30 13:17:33 -0300

    V 0.3

--- assets/js/todasMaterias.js
+++ assets/js/todasMaterias.js
@@ -2,8 +2,6 @@ const materias = ['Lingua-Inglesa', 'Lingua-Portuguesa', 'Geografia', 'História
     'Analise e Proj de Sist', 'Ciência da Comp', 'Banco de Dados', 'Prog. Front-end', 'Prog. Mobile', 'Proj de Vida', 'Educ Financeira'];
 
 const matDiv = document.querySelector('.materias');
-const btnAbrirMat = document.querySelector('#btnAbrirMat');
-const btnFechaMat = document.querySelector('#btnFechaMat');
 
 function mostrarMaterias() {
     for (let i = 0; i < materias.length; i++) {
