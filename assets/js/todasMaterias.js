116ce1a8f0cfda4a2f8c1f3ef331e29fe2a18af6
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2023-01-30 13:32:59 -0300

    V 0.5

--- assets/js/todasMaterias.js
+++ assets/js/todasMaterias.js
@@ -3,10 +3,36 @@ const materiasArray = ['Lingua-Inglesa', 'Lingua-Portuguesa', 'Geografia',
     'Analise e Proj de Sist', 'Ciência da Comp', 'Banco de Dados',
     'Prog. Front-end', 'Prog. Mobile', 'Proj de Vida', 'Educ Financeira'];
 
-for (let i = 0; i < materiasArray.length; i++) {
-    const materia = materiasArray[i];
-    const div = document.createElement('div');
-    div.classList.add('materias');
-    div.textContent = materia;
-    document.body.appendChild(div);
-}
+let isArrayOpened = false;
+
+const createMaterias = () => {
+    for (let i = 0; i < materiasArray.length; i++) {
+        const materia = materiasArray[i];
+        const div = document.createElement('div');
+        div.classList.add('materias');
+        div.textContent = materia;
+        document.body.appendChild(div);
+    }
+};
+
+const btnOpenArray = document.createElement('button');
+btnOpenArray.textContent = 'Abrir Array';
+btnOpenArray.addEventListener('click', () => {
+    if (!isArrayOpened) {
+        createMaterias();
+        isArrayOpened = true;
+    }
+});
+document.body.appendChild(btnOpenArray);
+
+const btnCloseArray = document.createElement('button');
+btnCloseArray.textContent = 'Fechar Array';
+btnCloseArray.addEventListener('click', () => {
+    if (isArrayOpened) {
+        document.querySelectorAll('.materias').forEach(element => {
+            element.remove();
+        });
+        isArrayOpened = false;
+    }
+});
+document.body.appendChild(btnCloseArray);
