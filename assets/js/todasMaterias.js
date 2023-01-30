108ab1e0a72a30977d264696d031db1074edc683
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2023-01-30 14:07:54 -0300

    ok

--- assets/js/todasMaterias.js
+++ assets/js/todasMaterias.js
@@ -1,40 +1,36 @@
-function meuEscopoTdsMat() {
+const materias = ['Lingua-Inglesa', 'Lingua-Portuguesa', 'Geografia', 'História', 'Sociologia', 'Matematica', 'Quimica', 'Biologia',
+    'Analise e Proj de Sist', 'Ciência da Comp', 'Banco de Dados', 'Prog. Front-end', 'Prog. Mobile', 'Proj de Vida', 'Educ Financeira'];
 
-    const materias = ['Lingua-Inglesa', 'Lingua-Portuguesa', 'Geografia', 'História', 'Sociologia', 'Matematica', 'Quimica', 'Biologia',
-        'Analise e Proj de Sist', 'Ciência da Comp', 'Banco de Dados', 'Prog. Front-end', 'Prog. Mobile', 'Proj de Vida', 'Educ Financeira'];
+const matDiv = document.querySelector('.materias');
 
-    const matDiv = document.querySelector('.materias');
+function limparDiv(div) {
+    div.innerHTML = "";
+}
 
-    function limparDiv(div) {
-        div.innerHTML = "";
+function mostrarMaterias() {
+    for (let i = 0; i < materias.length; i++) {
+        const newContent = materias[i];
+        const newP = document.createElement("h3");
+        const newText = document.createTextNode(newContent);
+        newP.appendChild(newText);
+        matDiv.appendChild(newP);
     }
+}
 
-    function mostrarMaterias() {
-        for (let i = 0; i < materias.length; i++) {
-            const newContent = materias[i];
-            const newP = document.createElement("h3");
-            const newText = document.createTextNode(newContent);
-            newP.appendChild(newText);
-            matDiv.appendChild(newP);
-        }
-    }
+function criaBotaoFechaMat() {
+    const newButton = document.createElement("button");
+    newButton.className = "btn-fecha-mat"
+    const buttonText = document.createTextNode("Fechar materias");
+    newButton.appendChild(buttonText);
+    newButton.onclick = () => matDiv.innerHTML = "";
+    document.querySelector(".materias").appendChild(newButton);
+}
 
-    function criaBotaoFechaMat() {
-        const newButton = document.createElement("button");
-        newButton.className = "btn-fecha-mat"
-        const buttonText = document.createTextNode("Fechar materias");
-        newButton.appendChild(buttonText);
-        newButton.onclick = () => matDiv.innerHTML = "";
-        document.querySelector(".materias").appendChild(newButton);
-    }
+function verMateriasBtn() {
+    limparDiv(matDiv)
+    mostrarMaterias()
+    criaBotaoFechaMat()
+    alert(`oi`)
+}
 
-    function verMateriasBtn() {
-        limparDiv(matDiv)
-        mostrarMaterias()
-        
-        criaBotaoFechaMat()
 
-        alert(`0.2ok`)
-    }
-
-} meuEscopoTdsMat()
